cc5df8c74c4493962b73d0a15a9afbe0d6073f7d
Jose Pino <pino.jose@gmail.com>
2015-05-04 13:43:19 -0430

    Aregada verificación de estatus del servidor remoto (off/online)

--- getrss.php
+++ getrss.php
@@ -2,12 +2,16 @@
   $urls = array("general"=>"http://prensa.ula.ve/feed", "viceadmin"=>"http://prensa.ula.ve/taxonomy/term/6/feed", "rectorado"=>"http://prensa.ula.ve/taxonomy/term/11/feed");
 
   foreach($urls as $x => $x_value) {
-    echo "Key=" . $x . ", Value=" . $x_value;
-    echo "<br>";
     $ch = curl_init($x_value);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_TIMEOUT, 50);
     $output = curl_exec($ch);
+    // Verificar que el servidor remoto esté online antes de escribir el archivo
+    $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if (200 <> $retcode) {
+      echo "Offline";
+      exit();
+    }
     curl_close($ch);
     file_put_contents('/var/www/html/rss/'.$x.'.rss', $output);
   }
